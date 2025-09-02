9560bfb05b273b6e49ec23cc52dcf3204dd87ccb
chaiwat kong_in <nickolas18237@gmail.com>
2025-09-02 17:58:54 +0700

    Create HW.6-5_While.c

---
+++ LAP 006/HW.6-5_While.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n , i , j , isPrime ;
+
+    printf( "กรอกจำนวนเต็ม: ")  ;
+    scanf( "%d" , &n ) ;
+
+    i = n ;
+    while( i >= 2 ) {
+        isPrime = 1 ; // สมมติว่าเป็นจำนวนเฉพาะ
+        j = 2 ;
+
+        // ตรวจสอบตัวหารโดยใช้ while
+        while( j < i ) {
+            if( i % j == 0 ) {
+                isPrime = 0 ; // ถ้าหารลงตัว → ไม่ใช่จำนวนเฉพาะ
+                break ;
+            }
+            j++ ;
+        }
+
+        if( isPrime ) {
+            printf( "%d ", i ) ;
+        }
+
+        i-- ; // ลดค่าลง
+    } // end while
+
+    return 0 ;
+}// end main
