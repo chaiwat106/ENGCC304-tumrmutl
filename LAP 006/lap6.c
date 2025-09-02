6928b583f76a29c23fdbaed626fa357cc03fc588
chaiwat kong_in <nickolas18237@gmail.com>
2025-09-02 15:47:11 +0700

    Update lap6.c

--- LAP 006/lap6.c
+++ LAP 006/lap6.c
@@ -11,12 +11,13 @@ int main() {
         for( int i = 1 ; i <= N ; i++ ) {
             printf( "%d\n" , i ) ;
             }
-    } else {
+        } else {
         printf( "Series Number : %d\n " , N ) ;
-        for( int i = 2 ; i <= N ; i +=2 ) {
+        for( int i = 2 ; i <= N ; i += 2 ) {
             printf( "%d\n" , i ) ;
         }
     }
 
-    return 0;
+    return 0 ;
 }
+
